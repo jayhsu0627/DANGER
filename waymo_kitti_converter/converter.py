8be0e616d462da86f00b72d9a137aca4ca26817a
jayhsu0627 <31528202+jayhsu0627@users.noreply.github.com>
2023-04-12 11:07:07 -0700

    init

--- waymo_kitti_converter/converter.py
+++ waymo_kitti_converter/converter.py
@@ -628,7 +628,7 @@ class WaymoToKITTI(object):
             if bounding_box == None or name == None:
                 name = '0'
                 bounding_box = (0, 0, 0, 0)
-            
+            print(id, name, type(name))
             if name != '0': continue  # output first camera only; Ignore other cameras
 
             my_type = self.type_list[obj.type]
