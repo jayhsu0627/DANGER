dcdf27f23f0976d13d9664cd074702aa56b636e6
jayhsu0627 <31528202+jayhsu0627@users.noreply.github.com>
2023-04-21 15:28:05 -0700

    init

--- install.sh
+++ install.sh
@@ -27,6 +27,8 @@ conda config --append channels conda-forge
 conda env create --name 3dsdn --file /pvc-ssd/Danger_model/DANGER/3D-SDN/environment.yml
 conda env list
 eval "$(conda shell.bash hook)"
+conda activate 3dsdn
+conda install pytorch==1.7.0 torchvision==0.8.0 torchaudio==0.7.0 -c pytorch
 
 
 echo "Install gcc-6 "
