e0e15855b7b9174ccf291805930f08dcff81c91a
jayhsu0627 <31528202+jayhsu0627@users.noreply.github.com>
2023-04-21 21:03:25 -0700

    init

--- install.sh
+++ install.sh
@@ -51,9 +51,9 @@ echo "Install pytorch and cuda 9.0 "
 
 wget https://developer.nvidia.com/compute/cuda/9.0/Prod/local_installers/cuda-repo-ubuntu1604-9-0-local_9.0.176-1_amd64-deb
 sudo dpkg -i cuda-repo-ubuntu1604-9-0-local_9.0.176-1_amd64-deb
-apt-key add /var/cuda-repo-9-0-local/7fa2af80.pub
-apt-get update
-apt-get install cuda=9.0.176-1
+sudo apt-key add /var/cuda-repo-9-0-local/7fa2af80.pub
+sudo apt-get update
+sudo apt-get install cuda=9.0.176-1
 
 export PATH=$PATH:/usr/local/cuda-9.0/bin
 export CUDADIR=/usr/local/cuda-9.0
