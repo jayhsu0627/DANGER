d8d593879a12941035bb9a31fa5fa24451879148
jayhsu0627 <31528202+jayhsu0627@users.noreply.github.com>
2023-04-22 19:12:23 -0700

    init

--- install.sh
+++ install.sh
@@ -65,10 +65,14 @@ echo "Install cuda 9.0 "
 
 # rm -rf cuda-repo-ubuntu1604-9-0-local_9.0.176-1_amd64-deb
 # rm -rf cuda-repo-ubuntu1704-9-0-176-local-patch-4_1.0-1_amd64-deb
-
 wget http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1704/x86_64/cuda-repo-ubuntu1704_9.0.176-1_amd64.deb
 sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1704/x86_64/7fa2af80.pub
+sudo apt-get clean
 sudo apt-get update
+sudo apt-get purge cuda
+sudo apt-get purge nvidia-*
+sudo apt-get autoremove
+
 sudo apt-get install cuda
 
 echo "Verification of cuda 9.0"
