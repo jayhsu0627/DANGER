3f8754a0316894215fde056673324d14c8999298
jayhsu0627 <31528202+jayhsu0627@users.noreply.github.com>
2023-04-23 14:15:56 -0700

    init

--- install.sh
+++ install.sh
@@ -49,11 +49,10 @@ rm -rf cuda-repo-ubuntu1704-9-0-local_9.0.176-1_amd64-deb || true
 rm -rf cuda-repo-ubuntu1704-9-0-local_9.0.176-1_amd64-deb.1 || true
 
 wget https://developer.nvidia.com/compute/cuda/9.0/Prod/local_installers/cuda-repo-ubuntu1604-9-0-local_9.0.176-1_amd64-deb
-sudo dpkg --configure -a
-dpkg -i cuda-repo-ubuntu1604-9-0-local_9.0.176-1_amd64-deb
+sudo dpkg -i cuda-repo-ubuntu1604-9-0-local_9.0.176-1_amd64-deb
 
 sudo apt-key add /var/cuda-repo-9-0-local/7fa2af80.pub
-sudo apt-get update
+# sudo apt-get update
 sudo apt-get install cuda=9.0.176-1
 
 export PATH=$PATH:/usr/local/cuda-9.0/bin
